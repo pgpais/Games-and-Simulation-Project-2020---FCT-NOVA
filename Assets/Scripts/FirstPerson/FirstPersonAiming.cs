4ab889be4fa68044b9b2a969a26f598a370cdf41
Pedro Pais <ppais95@gmail.com>
2020-03-17 16:34:58 +0000

    Updated to 2018.4.19f1
    Camera Clamp now functional.

--- Assets/Scripts/FirstPerson/FirstPersonAiming.cs
+++ Assets/Scripts/FirstPerson/FirstPersonAiming.cs
@@ -7,29 +7,46 @@ public class FirstPersonAiming : MonoBehaviour
 {
     public float sensitivity = 10f;
 
+    private float rotY;
+
+    [Header("Camera Properties")] 
+    public float maxClamp = 90.0f;
+    public float minClamp = -90.0f;
     private Camera cam;
     [SerializeField]
     private Transform camTransform;
+    private float rotX;
     
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         cam = camTransform.GetComponent<Camera>();
+        rotX = 0;
+        rotY = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float aimX = Input.GetAxisRaw("Mouse X");
-        float aimY = Input.GetAxisRaw("Mouse Y");
-        HandleAiming(aimX, aimY);
+        HandleAiming(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
     }
 
     void HandleAiming(float aimX, float aimY)
     {
-        transform.Rotate(Vector3.up, aimX*sensitivity);
+        // Add rotations
+        rotX -= aimY * sensitivity;
+        rotY += aimX * sensitivity;
+
+        // Clamp camera rotation so the player doesn't turn upside down
+        rotX = Mathf.Clamp(rotX, minClamp, maxClamp);
         
-        camTransform.Rotate(Vector3.left, aimY*sensitivity);
+        // Set rotations
+        Vector3 bodyRotation = transform.eulerAngles;
+        Vector3 camRotation = camTransform.localRotation.eulerAngles;
+        bodyRotation.y = rotY;
+        camRotation.x = rotX;
+        camTransform.localRotation = Quaternion.Euler(camRotation);
+        transform.eulerAngles = bodyRotation;
     }
 }
