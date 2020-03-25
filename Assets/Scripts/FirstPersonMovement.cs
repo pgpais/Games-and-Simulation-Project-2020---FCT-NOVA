e2ed28388db56e8b9ed32982f9386e126af6e83e
Pedro Pais <ppais95@gmail.com>
2020-03-25 09:36:53 +0000

    Added jumping and groundChecks

--- Assets/Scripts/FirstPersonMovement.cs
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -8,8 +8,22 @@ using UnityEngine;
 public class FirstPersonMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpForce = 10f;
+    
+    [SerializeField]
+    private bool isGrounded = true;
 
-    private float movH, movV; 
+    [Header("OverlapBox Points")]
+    [SerializeField]
+    private Transform GroundCheck;
+    private Vector3 boxCenter;
+    [SerializeField]
+    private Vector3 boxRadiuses;
+    [SerializeField]
+    private LayerMask boxMask;
+
+    private float movH, movV;
+    private bool triedJumping;
     private Rigidbody rb;
     
     // Start is called before the first frame update
@@ -23,11 +37,36 @@ public class FirstPersonMovement : MonoBehaviour
     {
         movH = Input.GetAxisRaw("Horizontal");
         movV = Input.GetAxisRaw("Vertical");
+        triedJumping = Input.GetButtonDown("Jump");
     }
 
     private void FixedUpdate()
     {
         HandleMovement();
+        HandleJumping();
+    }
+
+    private void HandleJumping()
+    {
+        CheckGrounded();
+        if (triedJumping && isGrounded)
+        {
+            // TODO: check different ForceModes for different feels
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void CheckGrounded()
+    {
+        if (Physics.OverlapBox(GroundCheck.position, boxRadiuses, Quaternion.identity, boxMask).Length > 0)
+        {
+            //TODO: check that this work for every case (probably mess with Layermask)
+            isGrounded = true; 
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
     void HandleMovement()
@@ -37,5 +76,13 @@ public class FirstPersonMovement : MonoBehaviour
         mov *= speed;
         mov += new Vector3(0, rb.velocity.y, 0);
         rb.velocity = mov;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        var position = GroundCheck.position;
+        Gizmos.DrawSphere(position, 0.05f);
+        Gizmos.DrawWireCube(position, boxRadiuses*2);
     }
 }
