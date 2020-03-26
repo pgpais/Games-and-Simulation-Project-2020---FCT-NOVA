2aafa2da7d688cac998e5bf7ce8535d7d245396d
João Rafael Pacheco <dblamk@hotmail.com>
2020-03-26 23:41:55 +0000

    simple script that molds to any respawn point and player

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawn.transform.position;
+        
+    }
+}
