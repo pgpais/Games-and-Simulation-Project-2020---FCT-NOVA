070a04949db1ff68274ee290b746024f96fc3f14
Pedro Pais <ppais95@gmail.com>
2020-03-27 09:36:08 +0000

    Created a skeleton of a Puzzle Room ScriptableObject

---
+++ Assets/Scripts/Puzzles/PuzzleRoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    [UnityEngine.CreateAssetMenu(fileName = "Puzzle Room", menuName = "Puzzle/Sample Room", order = 0)]
+    public class PuzzleRoom : UnityEngine.ScriptableObject
+    {
+        [Serializable]
+        public struct PathWays
+        {
+            public bool North;
+            public bool South;
+            public bool West;
+            public bool East;
+        }
+        
+        public Scene scene;
+
+        [Header("Properties")]
+        [Range(1, 5)]
+        public float difficulty;
+        [Tooltip("Position set to true if N, S, W, E exist, respectively")]
+        public PathWays pathWays;
+        
+    }
+
+    
+}
