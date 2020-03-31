a13dbdd975a80972f1d4868534d6833187caf2c3
Claudio Pereira <cn.pereira@campus.fct.unl.pt>
2020-03-31 14:43:36 +0000

    Added FlashLight and Button + Animation

---
+++ Assets/Scripts/FirstPerson/FirstPersonInteract.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace FirstPerson
+{
+    public class FirstPersonInteract : MonoBehaviour
+    {
+        public Tool tool;
+        
+        void Update()
+        {
+            if (Input.GetButtonDown("UseTool"))
+                tool.UseTool();
+                
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Tools/FlashLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : Tool
+{
+
+    [SerializeField] private Light flashLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashLight = flashLight == null ? GetComponentInChildren<Light>() : flashLight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void UseTool()
+    {
+        flashLight.enabled = !flashLight.enabled;
+    }
+}
