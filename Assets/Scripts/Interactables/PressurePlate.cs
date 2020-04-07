9b64e917b804a5fb490639fb34a88ba051b727a4
João Rafael Pacheco <dblamk@hotmail.com>
2020-04-07 14:41:45 +0000

    No Comments

--- Assets/Scripts/Interactables/PressurePlate.cs
+++ Assets/Scripts/Interactables/PressurePlate.cs
@@ -8,17 +8,31 @@ public class PressurePlate : MonoBehaviour
     [SerializeField] private GameObject door;
 
     private bool open = false;
-    
+    [SerializeField]
+    public GameObject box;
+    [SerializeField]
+    public Material wrong;
+    [SerializeField]
+    public Material right;
+    [SerializeField]
+    public Material normal;
     
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("I'm in");
         if (!open)
         {
-            open = true;
-            door.transform.position += new Vector3(0, 4, 0);
-            transform.GetComponent<Renderer>().material.color = Color.red;
-            
+            if (other.CompareTag("Carryable"))
+            {
+                open = true;
+                door.transform.position += new Vector3(0, 4, 0);
+                box.GetComponent<Renderer>().material = right;
+
+            }
+            else
+            {
+                box.GetComponent<Renderer>().material = wrong;
+            }
         }
     }
 
@@ -29,6 +43,8 @@ public class PressurePlate : MonoBehaviour
         {
             door.transform.position += new Vector3(0, -4, 0);
             open = false;
+            box.GetComponent<Renderer>().material = normal;
+
         }
     }
 }
