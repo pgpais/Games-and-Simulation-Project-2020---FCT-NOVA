a8ae2d9e62999738ad96ecafb84458d15c26f8e5
João Rafael Pacheco <dblamk@hotmail.com>
2020-04-07 15:08:16 +0000

    plate now changes color on activation, switch between allowing player to activate it or not
    add to refactor some folders be careful. Materials and textures went to resources to allow access with scripts

--- Assets/Scripts/Interactables/PressurePlate.cs
+++ Assets/Scripts/Interactables/PressurePlate.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PressurePlate : MonoBehaviour
 {
@@ -9,29 +10,26 @@ public class PressurePlate : MonoBehaviour
 
     private bool open = false;
     [SerializeField]
-    public GameObject box;
-    [SerializeField]
-    public Material wrong;
-    [SerializeField]
-    public Material right;
-    [SerializeField]
-    public Material normal;
+    public GameObject plate;
+
+    public bool allowPlayer = false;
     
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("I'm in");
         if (!open)
         {
-            if (other.CompareTag("Carryable"))
+            if (other.CompareTag("Carryable") || allowPlayer)
             {
                 open = true;
                 door.transform.position += new Vector3(0, 4, 0);
-                box.GetComponent<Renderer>().material = right;
+                plate.GetComponent<Renderer>().material = Resources.Load("Materials/GreenMat", typeof(Material)) as Material;;
 
             }
             else
             {
-                box.GetComponent<Renderer>().material = wrong;
+                plate.GetComponent<Renderer>().material = Resources.Load("Materials/RedMat", typeof(Material)) as Material;
             }
         }
     }
@@ -43,8 +41,9 @@ public class PressurePlate : MonoBehaviour
         {
             door.transform.position += new Vector3(0, -4, 0);
             open = false;
-            box.GetComponent<Renderer>().material = normal;
 
         }
+        
+        plate.GetComponent<Renderer>().material = Resources.Load("Materials/Grey", typeof(Material)) as Material;;
     }
 }
