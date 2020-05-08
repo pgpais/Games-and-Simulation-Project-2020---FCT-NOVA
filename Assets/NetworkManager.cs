209ba6991f646ace0b86bf5688d42c434c776548
Pedro Pais <ppais95@gmail.com>
2020-05-08 11:51:11 +0000

    Improved menu a bit. Will add lobby and first level after

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager instance;
+    
+    private string gameVersion = "1";
+    
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Tried to create a new NetworkManager. Please be sure that there is only one in the scene");
+            Destroy(this);
+            return;
+        }
+
+        instance = this;
+        Connect();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        MenuManager.instance.ActivateButtons();
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+}
--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,18 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MenuManager instance;
+
+    [SerializeField]
+    private List<GameObject> objectsToActivateOnConnection;
+    
+    [SerializeField]
+    private TMP_InputField roomNameInput;
+
+
+    private void Start()
     {
-        
+        if (instance != null)
+        {
+            Debug.LogWarning("Tried to create a new MenuManager. Please be sure that there is only one in the scene");
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateLobby()
     {
-        
+        if (roomNameInput.text.Equals(""))
+        {
+            Debug.Log("Trying to Host Lobby lobby");
+            //PhotonNetwork.CreateRoom("lobby"); //TODO: check if this works with nulls
+        }
+        else
+        {
+            Debug.Log("Trying to Host Lobby " + roomNameInput.text);
+            //PhotonNetwork.CreateRoom(roomNameInput.text); //TODO: check if this works with nulls
+        }
     }
+    
+    public void JoinLobby()
+    {
+        if (roomNameInput.text.Equals(""))
+        {
+            Debug.Log("Trying to Join Lobby lobby");
+            //PhotonNetwork.JoinRoom("lobby"); //TODO: check if this works with nulls
+        }
+        else
+        {
+            Debug.Log("Trying to Join Lobby " + roomNameInput.text);
+            //PhotonNetwork.JoinRoom(roomNameInput.text); //TODO: check if this works with nulls
+        }
+    }
+
+    public void ActivateButtons()
+    {
+        foreach (GameObject obj in objectsToActivateOnConnection)
+        {
+            obj.SetActive(true);
+        }
+    }
+    
 }
