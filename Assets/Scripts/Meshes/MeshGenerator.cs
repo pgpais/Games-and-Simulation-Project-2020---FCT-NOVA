8e2dc2e9f2e298b3e22b97b8debdc2440afcbfed
Pedro Pais <ppais95@gmail.com>
2020-05-13 17:45:51 +0000

    No Comments

--- Assets/Scripts/Meshes/MeshGenerator.cs
+++ Assets/Scripts/Meshes/MeshGenerator.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
-    public int length, width;
+    public int xSize = 20, zSize = 20;
     public int edgeLength;
+    public float noiseMultiplier = 2f;
+    public float perlinNoiseX = .3f, perlinNoiseY = .3f;
     
     private Mesh mesh;
     
     private Vector3[] vertices;
     private int[] triangles;
+    private Vector2[] uvs;
 
     // Start is called before the first frame update
     void Start()
@@ -29,25 +33,66 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = uvs;
         
         mesh.RecalculateNormals();
     }
 
     private void CreateShape()
     {
-        vertices = new Vector3[length * width];
-        for (int i = 0; i < length; i++)
+        vertices = new Vector3[(xSize+1) * (zSize+1)];
+        
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            for (int j = 0; j < width; j++)
+            for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(edgeLength*i, 0, edgeLength * j);
+                float y = Mathf.PerlinNoise(x * perlinNoiseX, z * perlinNoiseY) * noiseMultiplier;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
             }
         }
+
+        triangles = new int[xSize * zSize * 6];
         
-        triangles = new int[]
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < zSize; z++)
         {
-            0, 1, 2,
-            1, 3, 2
-        };
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                
+                vert++;
+                tris += 6;
+            }
+
+            vert++;
+        }
+        
+        uvs = new Vector2[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                uvs[i] = new Vector2((float)x / xSize, (float)z/zSize);
+                i++;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(vertices == null)
+            return;
+
+        foreach (var vertex in vertices)
+        {
+            Gizmos.DrawSphere(transform.position + vertex, .1f);
+        }
     }
 }
---
+++ Assets/Scripts/Meshes/RopeMeshGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeMeshGenerator : MonoBehaviour
+{
+    public int xSize = 20, zSize = 20, ySize = 1; // ySize = nSegments?
+    public int nSegments;
+    
+    
+    private Mesh mesh;
+    
+    private Vector3[] vertices;
+    private int[] triangles;
+    private Vector2[] uvs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        
+        mesh.RecalculateNormals();
+    }
+    
+    private void CreateShape()
+    {
+        // create vertices
+        vertices = new Vector3[(xSize+1) * (zSize+1) * (ySize+1)];
+
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int z = 0; z <= zSize; z++)
+            {
+                for (int x = 0; x <= xSize; x++)
+                {
+                    //float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 5f;
+                    vertices[i] = new Vector3(x, -y, z);
+                    i++;
+                }
+            }
+        }
+
+        
+        // create triangles
+        triangles = new int[xSize * zSize * ySize * 6];
+        
+        int vert = 0;
+        int tris = 0;
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int z = 0; z < zSize; z++)
+            {
+                for (int x = 0; x < xSize; x++)
+                {
+                    triangles[tris] = vert + 0;
+                    triangles[tris + 1] = vert + xSize + 1;
+                    triangles[tris + 2] = vert + 1;
+                    triangles[tris + 3] = vert + 1;
+                    triangles[tris + 4] = vert + xSize + 1;
+                    triangles[tris + 5] = vert + xSize + 2;
+
+                    vert++;
+                    tris += 6;
+                }
+
+                vert++;
+            }
+        }
+
+        uvs = new Vector2[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                uvs[i] = new Vector2((float)x / xSize, (float)z/zSize);
+                i++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        if(vertices == null)
+            return;
+
+        foreach (var vertex in vertices)
+        {
+            Gizmos.DrawSphere(transform.position + vertex, .1f);
+        }
+    }
+}
