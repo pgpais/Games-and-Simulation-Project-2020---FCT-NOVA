80bac8eb36f5e2374747ee2d959a266170873a2a
João Pacheco <dblamk@hotmail.com>
2020-05-16 17:31:48 +0100

    last level stuff

---
+++ Assets/EndLevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject floor;
+
+
+    public void destroyFloor()
+    {
+        Destroy(floor);
+    }
+}
