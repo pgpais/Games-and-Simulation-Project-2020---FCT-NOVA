147dadc7f5b2571d43ade875afffd3f5fbb1371f
João Pacheco <dblamk@hotmail.com>
2020-06-04 15:50:48 +0100

    more audio stuff

---
+++ Assets/HittingSound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HittingSound : MonoBehaviour
+{
+
+    public AudioSource collisionSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionSound = GetComponent<AudioSource>();
+    }
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        collisionSound.Play();
+    }
+}
