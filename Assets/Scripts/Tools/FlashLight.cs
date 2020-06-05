54306676eaeae2192faf979e71bd4436048f25aa
Cláudio Pereira <claudioRP_97@hotmail.com>
2020-06-05 13:35:29 +0100

    Chore(FlashLight): Added Click Sound

--- Assets/Scripts/Tools/FlashLight.cs
+++ Assets/Scripts/Tools/FlashLight.cs
@@ -6,11 +6,13 @@ using UnityEngine.InputSystem;
 public class FlashLight : Tool
 {
     [SerializeField] private Light flashLight;
+    private AudioSource _audio;
 
     // Start is called before the first frame update
     void Start()
     {
         flashLight = flashLight == null ? GetComponentInChildren<Light>() : flashLight;
+        _audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,8 +22,12 @@ public class FlashLight : Tool
 
     public override void UseTool(InputActionPhase phase)
     {
-        if (phase == InputActionPhase.Performed) 
+        if (phase == InputActionPhase.Performed)
+        {
             flashLight.enabled = !flashLight.enabled;
+            _audio.Play();
+        }
+            
     }
 
     public override void UseToolSecondary(InputActionPhase phase)
