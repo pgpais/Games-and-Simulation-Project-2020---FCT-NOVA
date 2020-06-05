12aee9969a2364f425fa9903a322f6cd4fe5daba
Cláudio Pereira <claudioRP_97@hotmail.com>
2020-06-05 13:13:43 +0100

    Chore(Background): Added Background music

---
+++ Assets/Scripts/Audio/BackGroundMusicInstance.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusicInstance : MonoBehaviour
+{
+    private static BackGroundMusicInstance _instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        if (_instance == null)
+            _instance = this;
+        else
+        {
+            Destroy(this);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+    
+}
